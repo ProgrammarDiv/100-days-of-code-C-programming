99a5200f8dd229754cf1613953148828fea3189e
Divyansh Kasera <80615068+ProgrammarDiv@users.noreply.github.com>
2025-12-01 11:31:44 +0530

    Add comment to describe circle area and circumference program
    Added a comment at the beginning of the file to describe the program's purpose.

--- Day02Q4.c
+++ Day02Q4.c
@@ -1,3 +1,5 @@
+//Write a program to calculate the area and circumference of a circle given its radius.
+
 #include <stdio.h>
 
 #define PI 3.14159
@@ -12,3 +14,4 @@ int main() {
     printf("Circumference = %.2f\n", circumference);
     return 0;
 }
+
